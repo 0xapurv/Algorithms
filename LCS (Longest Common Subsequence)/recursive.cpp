7529626d38e3ce425d71df9cddf02f95aaedc16c
Apurv <apurv.bmsit@gmail.com>
2021-02-25 12:13:54 +0530

    feat: recursive Code added

---
+++ LCS (Longest Common Subsequence)/recursive.cpp
@@ -0,0 +1,26 @@
+/*
+* Time Complexity ~ O(2^n)
+* Top to Bottom Approach
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+char A[] = "bd";
+char B[] = "abcd";
+
+int LCS(int i,int j) {
+    if(A[i]=='\0' || B[j]=='\0') 
+    return 0;
+
+    else  if(A[i]==B[j])
+    return 1 + LCS(i+1,j+1);
+
+    else
+    return max(LCS(i+1,j),LCS(i,j+1));
+}
+
+int main(){
+
+int res = LCS(0,0);
+printf("%d",res);
+}
